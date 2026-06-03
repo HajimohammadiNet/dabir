4f0bcaf4a205a1c4336f1f5363b1cfd674b6b65d
hajimohammadinet <info@hajimohammadi.net>
2026-06-03 12:12:38 +0330

    feat: add letter attachments persistence

---
+++ migrations/000008_create_letter_attachments_table.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_letter_attachments_created_at;
+DROP INDEX IF EXISTS idx_letter_attachments_letter_id;
+DROP INDEX IF EXISTS idx_letter_attachments_object_key_unique;
+
+DROP TABLE IF EXISTS letter_attachments;
